2d91dc92be5fb463c545875aeb744765c6b89cd2
Kate Kumpanska <kate.kumpansky@gmail.com>
2025-03-09 11:50:43 +0200

    add delegate and developer shaker sort

---
+++ task6dlg/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6dlg
+{
+    public delegate void SortingMethod(double[] arr);
+    class Sort
+    { 
+        public static void DeveloperShakerSort(double[]arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                double temp;
+                bool swap = false;
+                for (int j = i; j < arr.Length - 1; j++)
+                {
+                    if (arr[j + 1] < arr[j])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swap = true;
+                    }
+                }
+                for (int j = arr.Length - i - 2; j > i; j--)
+                {
+                    if (arr[j - 1] > arr[j])
+                    {
+                        temp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = temp;
+                        swap = true;
+                    }
+                }
+                if (swap == false)
+                { break; }
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
